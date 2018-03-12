7d1577ddcc25d1a67cbcdd463d1b6f607bccb411
kikogatto <gatto@gatto.com.br>
2018-03-12 15:41:34 -0500

    circleci

--- events-services/src/dispatcher.js
+++ events-services/src/dispatcher.js
@@ -3,6 +3,80 @@
 //----------------------------
 'use strict';
 
+/*
+var AWS = require("aws-sdk");
+var async = require('async');
+
+var INTAKE_QUEUE_URL = process.env.INTAKE_QUEUE_URL;
+var CATCHALL_QUEUE_URL = process.env.CATCHALL_QUEUE_URL;
+var AWS_REGION = process.env.AWS_REGION;
+
+var sqs = new AWS.SQS({region: AWS_REGION});
+
+var deleteMessage = function(receiptHandle, cb) {
+  var params = {
+    ReceiptHandle: receiptHandle,
+    QueueUrl: INTAKE_QUEUE_URL
+  };
+  sqs.deleteMessage(params, cb);
+};
+
+var dispatchEvent = function(event, queue, groupId, cb) {
+    var params = {
+    MessageGroupId: groupId,
+    MessageBody: event.Message,
+    QueueUrl: queue
+  };
+  sqs.sendMessage(params, cb);
+};
+
+var listSubscribers = function(eventType, callback) {
+  var params = {
+    QueueNamePrefix: eventType
+  };
+  sqs.listQueues( params, callback );
+};
+
+var dispatch = function(eventString, cb) {
+  var event = JSON.parse(eventString);
+  var invocations = [];
+  invocations.push(function(callback) {
+    dispatchEvent( event, CATCHALL_QUEUE_URL, "catchAll", callback);
+  });
+
+  listSubscribers( event.eventType, function(err, data) {
+    if(err) {
+       cb(err);
+    } else {
+      for(var i = 0; i< data.QueueUrls.length; i++ ) {
+        invocations.push(function(callback) {
+          dispatchEvent( event, data.QueueUrls[i], event.eventType, callback);
+        });
+      };
+
+      async.parallel(invocations, function(err, results) {
+        cb(err, results);
+      });
+    }
+  });
+};   
+
+exports.handler = function(message, context, callback) {
+  dispatch(message.Body, function(err, results) {
+    if (err) {
+       callback(err);
+    } else {
+      deleteMessage(message.ReceiptHandle, callback);
+    }
+  });
+};
+
+*/
+
+
+
+
+
 var AWS = require("aws-sdk");
 
 var INTAKE_QUEUE_URL = process.env.INTAKE_QUEUE_URL;
